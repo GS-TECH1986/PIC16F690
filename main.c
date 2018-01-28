a3539cfcf706473b16484e1b75d40700657d99ea
GS-TECH1986 <34194579+GS-TECH1986@users.noreply.github.com>
2018-01-28 12:20:16 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <main2.h>
+
+void main()
+{
+	setup_adc_ports(sAN0);
+	setup_adc(ADC_CLOCK_INTERNAL);
+	setup_timer_0(RTCC_INTERNAL|RTCC_DIV_1|RTCC_8_BIT);		//256 us overflow
+
+
+	while(TRUE)
+	{
+
+		//Example blinking LED program
+		output_low(LED);
+		delay_ms(DELAY);
+		output_high(LED);
+		delay_ms(DELAY);
+
+		//TODO: User Code
+	}
+
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#include <16F690.h>
+#device ADC=10
+#use delay(crystal=4MHz)
+
+#define LED PIN_A2
+#define DELAY 1000
+
+
